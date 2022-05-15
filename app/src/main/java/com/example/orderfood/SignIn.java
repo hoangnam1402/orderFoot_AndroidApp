1f76c93ac1a564adb408c3da5c485d5bb1e241bf
hoangnam1402 <18521130@gm.uit.edu.vn>
2022-05-15 23:39:53 +0700

    signin

---
+++ app/src/main/java/com/example/orderfood/SignIn.java
@@ -0,0 +1,68 @@
+package com.example.orderfood;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.orderfood.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignIn extends AppCompatActivity {
+    EditText editPhone, editPassword;
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        editPassword = (MaterialEditText) findViewById(R.id.editPassword);
+        editPhone = (MaterialEditText) findViewById(R.id.editPhone);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please waiting...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.child(editPhone.getText().toString()).exists()) {
+                            mDialog.dismiss();
+                            User user = snapshot.child(editPhone.getText().toString()).getValue(User.class);
+                            if (user.getPassword().equals(editPassword.getText().toString())) {
+                                Toast.makeText(SignIn.this, "Sign in successfully !", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignIn.this, "Sign in failed !!!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(SignIn.this, "User not exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+    }
+}
